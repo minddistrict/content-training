df7c0e1f21f8a15922fd760722055933045d540f
nielsbom <nbom@minddistrict.com>
2015-10-06 17:29:45 +0200

    Add averaging example.

---
+++ examples/0006_average/computation.js
@@ -0,0 +1,14 @@
+var computations = {
+    animal_liking_average:function(data){
+        var total;
+        var average;
+
+        total = 0;
+        total += data.horses;
+        total += data.cats;
+        total += data.dogs;
+
+        average = total / 3;
+        return average;
+    }
+};
