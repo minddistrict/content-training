996601f3347c9c00a9b7b8a1fb50a634c23ae391
nielsbom <nbom@minddistrict.com>
2015-10-07 18:32:30 +0200

    Make 0013 more simple.

--- examples/0013_rescaling/computation.js
+++ examples/0013_rescaling/computation.js
@@ -3,18 +3,9 @@ var computations = {
         var total;
         total = 0;
 
+
         var q2_rescaled; // will contain rescaled value
-        if (data.q2 === 1) {
-            q2_rescaled = 5;
-        } else if (data.q2 === 2) {
-            q2_rescaled = 4;
-        } else if (data.q2 === 3) {
-            q2_rescaled = 3;
-        } else if (data.q2 === 4) {
-            q2_rescaled = 2;
-        } else if (data.q2 === 5) {
-            q2_rescaled = 1;
-        }
+        q2_rescaled = 6 - data.q2;
 
         total = total + data.q1;
         total = total + q2_rescaled;
